fc68cf565ad853a9fdb172c09ebf67935bd236a9
radmanxu <radmanxu@icloud.com>
2017-08-04 23:46:40 +0800

    test on ubuntu

--- ubuntu_docker_oracle.sh
+++ ubuntu_docker_oracle.sh
@@ -5,23 +5,23 @@ apt-get install docker.io
 docker pull wnameless/oracle-xe-11g
 
 # 运行docker下的oracle
- ################################
- # 			                	#
- # oracle数据库连接对应信息:	#
- # 			                	#
- # hostname:外网ip	        	#
- # 			                	#
- # port: 49161		        	#
- # 			                   	#
- # sid: xe		            	#
- # 			                	#
- # username: system	        	#
- # 			                	#
- # password: oracle	        	#
- # 			                	#
- # SYS的密码: oracle    		#
- #			                	#
- ################################
+ ###############################
+ # 			              	
+ # oracle数据库连接对应信息:	
+ # 			              	
+ # hostname:外网ip	              
+ # 			        
+ # port: 49161		        
+ # 			        
+ # sid: xe		        
+ # 			        
+ # username: system	        
+ # 			        
+ # password: oracle	        
+ # 			        
+ # SYS的密码: oracle    	
+ #			        
+ ###############################
 docker run -h "oracle"  -d -p 49160:22 -p 49161:1521 -p 49162:8080 -e ORACLE_ALLOW_REMOTE=true wnameless/oracle-xe-11g
 
 # 添加docker到开机启动
@@ -33,7 +33,7 @@ sudo update-rc.d docker_oracle defaults 95
 cd -
 
 # 输出消息
-echo message
+cat message
 
 # 卸载开机启动
 # sudo update-rc.d -f docker_oracle remove
